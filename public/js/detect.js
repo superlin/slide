c210335421d893296128c81da5b51a678559b7e0
superlin <1092793900@qq.com>
2015-08-30 21:19:28 +0800

    添加serve test

---
+++ public/js/detect.js
@@ -0,0 +1,85 @@
+; (function (window, undefined) {
+  'use strict';
+  
+  var TEST_ELE = document.createElement('p');
+  var PREFIXS = ['', 'webkit', 'Moz', 'ms', 'O'];
+  
+  function prefixed(prop) {
+    var res = {},
+      camp = prop.slice(0, 1).toUpperCase() + prop.slice(1);
+    
+    prop = prop.toLowerCase();
+    
+    PREFIXS.forEach(function (pre) {
+      var key = pre === '' ? prop : pre+camp,
+        val =  pre === '' ? prop : '-'+pre.toLowerCase()+'-'+prop;
+      res[key] = val;
+    });
+    
+    return res;
+  }
+  
+  function propName(prop) {
+    var props = prefixed(prop);
+    for (var t in props) {
+      if (TEST_ELE.style[t] !== undefined) {
+        return {
+          key: t,
+          val: props[t]
+        };
+      }
+    }
+  }
+
+  var transformName = (function () {
+    return propName('transform');
+  })();
+  
+  var transitionName = (function () {
+    return propName('transition');
+  })();
+
+  var transitionEndName = (function () {
+    var i,
+      el = document.createElement('div'),
+      transitions = {
+        'transition': 'transitionend',
+        'webkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'msTransition': 'msTransitionEnd',
+        'OTransition': 'otransitionend'
+      };
+
+    for (i in transitions) {
+      if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+        return transitions[i];
+      }
+    }
+  })();
+
+  var transform3d = (function () {
+    if (!transformName || !window.getComputedStyle) {
+      return false;
+    }
+
+    var has3d;
+
+    document.body.insertBefore(TEST_ELE, null);
+
+    if (TEST_ELE.style[transformName.key] !== undefined) {
+      TEST_ELE.style[transformName.key] = 'translate3d(1px, 1px, 1px)';
+      has3d = window.getComputedStyle(TEST_ELE).getPropertyValue(transformName.val);
+    }
+
+    document.body.removeChild(TEST_ELE);
+
+    return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+  })();
+  
+  window.Detect = {
+    transitionName: transitionName,
+    transitionEndName: transitionEndName,
+    transformName: transformName,
+    transform3d: transform3d
+  };
+})(this);
---
+++ public/js/slide.js
@@ -0,0 +1,198 @@
+; (function (window, undefined) {
+
+  function $(selector, context) {
+    var eles;
+
+    if (typeof selector !== 'string') {
+      if (selector.length && selector.length !== 1) {
+        return [].slice.call(selector);
+      } else {
+        return [selector];
+      }
+    } else {
+      context = context || document;
+      eles = context.querySelectorAll(selector);
+      eles = eles || [];
+  
+      return [].slice.call(eles);
+    }
+  }
+
+  function bind(eles, events, fun) {
+    eles = $(eles);
+    events = events.split(/\s+/);
+
+    eles.forEach(function (ele) {
+      events.forEach(function (event) {
+        if (event) {
+          ele.addEventListener(event, fun);
+        }
+      });
+    });
+  }
+  
+  function unbind(eles, events, fun) {
+    eles = $(eles);
+    events = events.split(/\s+/);
+
+    eles.forEach(function (ele) {
+      events.forEach(function (event) {
+        if (event) {
+          ele.removeEventListener(event, fun);
+        }
+      });
+    });
+  }
+
+  var settings = {
+    interval: 3000
+  };
+
+  function Slide(opts) {
+    var self = this,
+      $container = $(opts.container)[0],
+      $list = $('.slide-list', $container)[0],
+      $prev = $('.slide-prev', $container)[0],
+      $next = $('.slide-next', $container)[0],
+      $markerWrap = $('.slide-marker', $container)[0],
+      $markers = $('span', $markerWrap),
+      $first, $last;
+
+    // 参数初始化
+    self.width = opts.width;
+    self.markers = $markers;
+    self.index = 1;
+    self.len = $markers.length;
+    self.animated = false;
+    self.interval = 3000;
+    
+    // 上一页和下一页
+    $prev.onclick = function () {
+      self.nextSlice();
+    };
+
+    $next.onclick = function () {
+      self.prevSlice();
+    };
+
+    // 初始化slide-marker
+    $markers[0].classList.add('active');
+    $markers.forEach(function (marker, i) {
+      marker.setAttribute('index', i + 1);
+    });
+
+    $markerWrap.onclick = function (e) {
+      var ele = e.target, index;
+      if (ele.tagName === 'SPAN') {
+        index = ele.getAttribute('index') || 1;
+        self.switchTo(parseInt(index, 10));
+      }
+    }
+  
+    // 初始化slide
+    $first = $('img:first-child', $list)[0];
+    $last = $('img:last-child', $list)[0];
+    $list.appendChild($first.cloneNode());
+    $list.insertBefore($last.cloneNode(), $first);
+
+    self.list = $list;
+    self.setPosition = setPosition.bind(self);
+    self.setPosition(-opts.width, 1, true);
+  
+    // 自动轮播
+    $container.onmouseover = function () {
+      self.stop();
+    };
+    $container.onmouseout = function () {
+      self.play();
+    };
+    self.play();
+  }
+
+  /**
+   * 设置下方第i个marker为active
+   * @param {Array}   所有的marker
+   * @param {Integer} 活动的下标
+   */
+  function setMarker($markers, i) {
+    $markers.forEach(function ($marker) {
+      var cls = $marker.classList;
+      if (cls.contains('active')) {
+        cls.remove(cls);
+        return false;
+      }
+    });
+    $markers[i].classList.add('active');
+  }
+
+  function setPosition(pos, slice, noTtransition) {
+    var self = this,
+      $ele = self.list,
+      transition = Detect.transitionName,
+      transform = Detect.transformName;
+
+    if (noTtransition) {
+      $ele.style[transition.key] = 'none';
+    } else {
+      self.animated = true;
+      $ele.style[transition.key] = transform.val + ' 0.5s linear';
+      bind($ele, Detect.transitionEndName, transitionend);
+    }
+    $ele.style[transform.key] = Detect.transform3d ? 'translate3d(' + pos + 'px, 0, 0)' : 'translate(' + pos + 'px, 0)';
+
+    function transitionend() {
+      var dis;
+      if (slice > self.len || slice < 1) {
+        if (slice > self.len) {
+          dis = -self.width;
+        } else if (slice < 1) {
+          dis = -self.len * self.width;
+        }
+        $ele.style[transition.key] = 'none';
+        $ele.style[transform.key] = Detect.transform3d ? 'translate3d(' + dis + 'px, 0, 0)' : 'translate(' + dis + 'px, 0)';;
+      }
+      unbind($ele, Detect.transitionEndName, transitionend);
+      self.animated = false;
+    }
+  }
+
+  Slide.prototype.switchTo = function (slice) {
+    if (this.animated || this.index === slice) {
+      return;
+    }
+    var newSlice = slice > this.len ? 1 : (slice < 1 ? this.len : slice);
+
+    this.index = newSlice;
+    setMarker(this.markers, this.index - 1);
+
+    this.setPosition(-slice * this.width, slice);
+  };
+
+  Slide.prototype.nextSlice = function () {
+    this.switchTo(this.index + 1);
+  };
+
+  Slide.prototype.prevSlice = function () {
+    this.switchTo(this.index - 1);
+  };
+
+  Slide.prototype.stop = function () {
+    clearTimeout(this.timer);
+  };
+
+  Slide.prototype.play = function () {
+    var self = this;
+    self.timer = setTimeout(function () {
+      self.nextSlice();
+      self.play();
+    }, self.interval);
+  };
+
+  window.Slide = Slide;
+
+})(this);
+
+var slide = new Slide({
+  container: '.container',
+  width: 750
+});
